15dd1bc38dc39f045d4b81b574ccd7b8fb4eb2ae
akk-hrt <akkhrtaus@yahoo.com>
2026-09-17 15:46:06 +0900

    Add event listeners for number buttons to javascript.js

--- javascript.js
+++ javascript.js
@@ -23,15 +23,6 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-/* 
-
-variables for operation
-----------------------------
-
-*/
-let operator = "";
-let num1 = 0;
-let num2 = 0;
 
 function operate(num1, operator, num2){
     if (operator === "+") {
@@ -47,3 +38,86 @@ function operate(num1, operator, num2){
     }
 
 }
+
+/* 
+
+variables for operation
+----------------------------
+
+*/
+let operator = "";
+let num1;
+let num2;
+let entered = "";
+
+const display = document.getElementById("display");
+
+const zero = document.getElementById("0");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+
+const clearBtn = document.getElementById("clear");
+
+/* 
+    <button id="add">+</button>
+                <button id="subtract">-</button>
+                <button id="multiply" aria-label="times">x</button>
+                <button id="divide">/</button>
+                <button id="equal">=</button>
+                <button id="clear">Clear</button>
+*/
+
+one.addEventListener("click", () => {
+    display.textContent += "1";
+})
+
+two.addEventListener("click", () => {
+    display.textContent += "2";
+})
+
+three.addEventListener("click", () => {
+    display.textContent += "3";
+})
+
+four.addEventListener("click", () => {
+    display.textContent += "4";
+})
+
+five.addEventListener("click", ()=> {
+    display.textContent += "5";
+})
+
+six.addEventListener("click", ()=>{
+    display.textContent += "6";
+})
+
+seven.addEventListener("click", () => {
+    display.textContent += "7";
+})
+
+eight.addEventListener("click", () => {
+    display.textContent += "8";
+})
+
+nine.addEventListener("click", () => {
+    display.textContent += "9";
+})
+
+zero.addEventListener("click", ()=> {
+
+    display.textContent += "0";
+})
+
+clearBtn.addEventListener("click", ()=>{
+    display.textContent = "";
+})
+
+
+
