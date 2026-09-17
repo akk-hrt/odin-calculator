4afeacad2eda67581a56c325fd1e4a11e3894b85
akk-hrt <akkhrtaus@yahoo.com>
2026-09-17 14:59:35 +0900

    Add basic math operation functions to javascript.js

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;    
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        return "No, you can't divide by 0!"
+    }
+
+    return num1 / num2;
+}
+
+
