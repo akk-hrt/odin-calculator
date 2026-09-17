25f37c0bd2919fd943e35ee1dc2b0f861634b0e9
akk-hrt <akkhrtaus@yahoo.com>
2026-09-17 15:11:47 +0900

    Add operate function to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+/* 
+Basic math functions
+------------------------------------
+*/
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -18,4 +23,27 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+/* 
 
+variables for operation
+----------------------------
+
+*/
+let operator = "";
+let num1 = 0;
+let num2 = 0;
+
+function operate(num1, operator, num2){
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2) 
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2)
+    } else {
+        return "Hooray!";
+    }
+
+}
